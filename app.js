6a41afa85dd4434f100250273d7407a13d8b3e9e
warren <warren.helland@hyparec.com>
2017-03-16 08:25:52 -0400

    adding new files

--- app.js
+++ app.js
@@ -29,271 +29,158 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
+
 //=========================================================
-// Bot Set the stage
+// Bot Setuo the bot object
 //=========================================================
+
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', [
+//=========================================================
+// Bot Set the intent object
+//=========================================================
+
+var intents = new builder.IntentDialog();
+
+
+//=========================================================
+// Bot path - intro and customer interest identification
+//=========================================================
+
+var bot = new builder.UniversalBot(connector, [
     function (session) {
-    	createHeroCard(session);
-        //session.beginDialog('/startSession', session.userData.profile);
-    },
+        // Send a greeting and show help.
+        var card = new builder.HeroCard(session)
+            .title("Hairhaus - London Ontario")
+            .images([
+                 builder.CardImage.create(session, "http://hairhaus.ca/images/hair-haus-logo-white.jpg")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
 
-    // Finish up
-    function (session, results) {
-        session.userData.profile = results.response;
-        session.send('Hello %(name)s! I love %(company)s!', session.userData.profile);
-    }
-]);
-
-//=========================================================
-// Bot path - intro and customer identification
-//=========================================================
-bot.dialog('/startSession', [
-    function (session, args, next) {
-        session.dialogData.profile = args || {};
-        if (session.dialogData.profile.name) {
-            builder.Prompts.text(session, "Hello... Welcome to Harihaus!  What would you like to do today?");
-            
-            session.userData.coding = results.response;
-        	
-        	builder.Prompts.choice(session, "What would you like to do at the spa?", 
-            ["Manicures", 
-            "Pedicures",
-            "Facials",
-            "Eyebrow Shaping",
-            "Brow Tint",
-            "Lash Tint",
-            "Hair Removal",
+        builder.Prompts.choice(session, "Hello... Welcome to Hairhaus!  How can we help you today?", [
+        	 "Cut and style", 
+            "Blow out and style",
+            "Formal Style",
+            "Colour",
+            "Highligts & Lowlights",
+            "Manicure & Pedicure",
+            "Facials & Massage",
+            "Beauty",
             "Waxing",
-            "Threading",
-            "Make Up"
-            ]
-            );
-        } else {
-            next();
-        }
-    },
-    function (session, results, next) {
-        if (results.response) {
-            session.dialogData.profile.name = results.response;
-        }
-        if (!session.dialogData.profile.company) {
-            builder.Prompts.text(session, "What company do you work for?");
-        } else {
-            next();
-        }
+            "Tint"
+        	]);
     },
     function (session, results) {
-        if (results.response) {
-            session.dialogData.profile.company = results.response;
-        }
-        session.endDialogWithResult({ response: session.dialogData.profile });
+        session.userData.serviceChoice = results.response.entity;
+        session.send("Got it... " + session.userData.serviceChoice + 
+                     ". Lets get an appt booked for you.");
+        session.beginDialog('/products');
     }
+]);
+
+
+bot.dialog('/banner', [
+    function (session) {
+        // Send a greeting and show help.
+        var card = new builder.HeroCard(session)
+            .title("Hairhaus - London Ontario")
+            .images([
+                 builder.CardImage.create(session, "http://hairhaus.ca/images/hair-haus-logo-white.jpg")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
+    }
+]);
+
+bot.dialog('/done', [
+    function (session) {
+        // Send a greeting and show help.
+        var card = new builder.HeroCard(session)
+            .title("Hairhaus - London Ontario")
+            .images([
+                 builder.CardImage.create(session, "http://hairhaus.ca/images/hair-haus-logo-white.jpg")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
+        session.beginDialog('/products');
+    }
+]);
+
+
+bot.dialog('/products', [
+	function (session) {
+
+		var cards = getCardsAttachments();
+
+    // create reply with Carousel AttachmentLayout
+    var reply = new builder.Message(session)
+        .attachmentLayout(builder.AttachmentLayout.carousel)
+        .attachments(cards);
+
+    session.send(reply);
+	}
+
 ]);
 
 
 //=========================================================
-// Bot Setup
+// Functions
 //=========================================================
-bot.dialog('/header', [
-    function (session) {
-        builder.Prompts.text(session, 'Image will be here - >');
-    }
-]);
 
-bot.dialog('/welcome', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi there! Welocme to the Harihaus.  What do you want to do today?');
-    }
-]);
-
-bot.dialog('/choiceMenu', [
-    function (session) {
-        builder.Prompts.text(session, '1, 2, 3, 4');
-    }
-]);
-
-bot.dialog('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
-    }
-]);
+function getCardsAttachments(session) {
+    return [
 
 
+        new builder.HeroCard(session)
+            .title('JAYDANCIN')
+            //.subtitle('Process events with a serverless code architecture')
+            .text('We are very excited to announce that we are now carrying Jaydancin products. They are ALL natural and organic , no preservatives or parabens. After A long search for products we really believe in, we finally found it.')
+            .images([
+                builder.CardImage.create(session, 'http://hairhaus.ca/images/jandancin.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'http://hairhaus.ca/', 'Learn More')
+            ]),
 
+        new builder.HeroCard(session)
+            .title('GOLDWELL')
+            //.subtitle('Process events with a serverless code architecture')
+            .text('For over 60 years Goldwell has been established as an exclusive luxury line to professional hairstylists. We are proud to partner with Goldwell and deliver optimal results to our guests.')
+            .images([
+                builder.CardImage.create(session, 'http://hairhaus.ca/images/promotion-goldwell.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'http://hairhaus.ca/', 'Learn More')
+            ]),
+
+        new builder.HeroCard(session)
+            .title('LAYRITE')
+            //.subtitle('Process events with a serverless code architecture')
+            .text('Layrite strives to represent a culture and brand that puts a modern style on a classic look. The masculine, vintage products they produce allow our customers to maintain this style while also adding their own originality to their overall image.')
+            .images([
+                builder.CardImage.create(session, 'http://hairhaus.ca/images/promo-layrite.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'http://hairhaus.ca/', 'Learn More')
+            ]),
+
+        new builder.HeroCard(session)
+            .title('FROM LONDON!')
+            //.subtitle('Process events with a serverless code architecture')
+            .text('Jaydancin offers a line of products that are created from the heart and human spirit, inspired from real people who have serious illnesses such as Cancer to people who simply want pure, clean products without chemicals.')
+            .images([
+                builder.CardImage.create(session, 'http://hairhaus.ca/images/promotion1.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'http://hairhaus.ca/', 'Learn More')
+            ]),
+
+    ];
+}
 
 //=========================================================
 // Wiring
 //=========================================================
+
 server.post('/api/messages', connector.listen());
-
-
-function createCard(selectedCardName, session) {
-    switch (selectedCardName) {
-        case HeroCardName:
-            return createHeroCard(session);
-        case ThumbnailCardName:
-            return createThumbnailCard(session);
-        case ReceiptCardName:
-            return createReceiptCard(session);
-        case SigninCardName:
-            return createSigninCard(session);
-        case AnimationCardName:
-            return createAnimationCard(session);
-        case VideoCardName:
-            return createVideoCard(session);
-        case AudioCardName:
-            return createAudioCard(session);
-        default:
-            return createHeroCard(session);
-    }
-}
-
-function createHeroCard(session) {
-    return new builder.HeroCard(session)
-        .title('BotFramework Hero Card')
-        .subtitle('Your bots — wherever your users are talking')
-        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
-        .images([
-            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
-        ])
-        .buttons([
-            builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')
-        ]);
-}
-
-function createThumbnailCard(session) {
-    return new builder.ThumbnailCard(session)
-        .title('BotFramework Thumbnail Card')
-        .subtitle('Your bots — wherever your users are talking')
-        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
-        .images([
-            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
-        ])
-        .buttons([
-            builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')
-        ]);
-}
-
-var order = 1234;
-function createReceiptCard(session) {
-    return new builder.ReceiptCard(session)
-        .title('John Doe')
-        .facts([
-            builder.Fact.create(session, order++, 'Order Number'),
-            builder.Fact.create(session, 'VISA 5555-****', 'Payment Method')
-        ])
-        .items([
-            builder.ReceiptItem.create(session, '$ 38.45', 'Data Transfer')
-                .quantity(368)
-                .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png')),
-            builder.ReceiptItem.create(session, '$ 45.00', 'App Service')
-                .quantity(720)
-                .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png'))
-        ])
-        .tax('$ 7.50')
-        .total('$ 90.95')
-        .buttons([
-            builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/pricing/', 'More Information')
-                .image('https://raw.githubusercontent.com/amido/azure-vector-icons/master/renders/microsoft-azure.png')
-        ]);
-}
-
-function createSigninCard(session) {
-    return new builder.SigninCard(session)
-        .text('BotFramework Sign-in Card')
-        .button('Sign-in', 'https://login.microsoftonline.com');
-}
-
-function createAnimationCard(session) {
-    return new builder.AnimationCard(session)
-        .title('Microsoft Bot Framework')
-        .subtitle('Animation Card')
-        .image(builder.CardImage.create(session, 'https://docs.botframework.com/en-us/images/faq-overview/botframework_overview_july.png'))
-        .media([
-            { url: 'http://i.giphy.com/Ki55RUbOV5njy.gif' }
-        ]);
-}
-
-function createVideoCard(session) {
-    return new builder.VideoCard(session)
-        .title('Big Buck Bunny')
-        .subtitle('by the Blender Institute')
-        .text('Big Buck Bunny (code-named Peach) is a short computer-animated comedy film by the Blender Institute, part of the Blender Foundation. Like the foundation\'s previous film Elephants Dream, the film was made using Blender, a free software application for animation made by the same foundation. It was released as an open-source film under Creative Commons License Attribution 3.0.')
-        .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg'))
-        .media([
-            { url: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4' }
-        ])
-        .buttons([
-            builder.CardAction.openUrl(session, 'https://peach.blender.org/', 'Learn More')
-        ]);
-}
-
-function createAudioCard(session) {
-
-	function getCardsAttachments(session) {
-    return [
-        new builder.HeroCard(session)
-            .title('Azure Storage')
-            .subtitle('Offload the heavy lifting of data center management')
-            .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
-            .images([
-                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/storage/media/storage-introduction/storage-concepts.png')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
-            ]),
-
-        new builder.ThumbnailCard(session)
-            .title('DocumentDB')
-            .subtitle('Blazing fast, planet-scale NoSQL')
-            .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
-            .images([
-                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/documentdb/media/documentdb-introduction/json-database-resources1.png')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
-            ]),
-
-        new builder.HeroCard(session)
-            .title('Azure Functions')
-            .subtitle('Process events with a serverless code architecture')
-            .text('An event-based serverless compute experience to accelerate your development. It can scale based on demand and you pay only for the resources you consume.')
-            .images([
-                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-5daae9212bb433ad0510fbfbff44121ac7c759adc284d7a43d60dbbf2358a07a/images/page/services/functions/01-develop.png')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
-            ]),
-
-        new builder.ThumbnailCard(session)
-            .title('Cognitive Services')
-            .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
-            .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
-            .images([
-                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-68b530dac63f0ccae8466a2610289af04bdc67ee0bfbc2d5e526b8efd10af05a/images/page/services/cognitive-services/cognitive-services.png')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/cognitive-services/', 'Learn More')
-            ])
-    ];
-}
-    return new builder.AudioCard(session)
-        .title('I am your father')
-        .subtitle('Star Wars: Episode V - The Empire Strikes Back')
-        .text('The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leigh Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing the film\'s story and serving as executive producer. The second installment in the original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. and stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.')
-        .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg'))
-        .media([
-            { url: 'http://www.wavlist.com/movies/004/father.wav' }
-        ])
-        .buttons([
-            builder.CardAction.openUrl(session, 'https://en.wikipedia.org/wiki/The_Empire_Strikes_Back', 'Read More')
-        ]);
-}
-
