08eb0f51bf129f79a9e1f1a956c55a13e2f13e6b
warren <warren.helland@hyparec.com>
2017-05-03 00:01:18 -0400

    updating/adding files

--- messages/index.js
+++ messages/index.js
@@ -8,7 +8,7 @@
 
 const restify = require('restify');
 require('dotenv').config();
-const bot = require('./bot.js');
+//const bot = require('./bot.js');
 
 const server = restify.createServer();
 server.post('/api/messages', bot.connector('*').listen());
