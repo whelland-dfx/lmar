0df7e9651d464f2489e2339ac07d67c566c549b0
warren <warren.helland@hyparec.com>
2017-05-04 23:33:04 -0400

    updating/adding files

--- messages/index.js
+++ messages/index.js
@@ -1,117 +1,43 @@
 
-//=========================================================
-// Setup All requires, vars and constants
-//=========================================================
+var builder = require("botbuilder");
 
-//'use strict';
-
-const restify = require('restify');
-require('dotenv').config();
-var builder = require('botbuilder');
-var botbuilder_azure = require('botbuilder-azure');
-
-
-//=========================================================
-// Plumbing
-//=========================================================
-
-var useEmulator = (process.env.NODE_ENV == 'development');
-
-const connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MICROSOFT_APP_ID'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    stateEndpoint: process.env['BotStateEndpoint'],
-    openIdMetadata: process.env['BotOpenIdMetadata']
+// Setup bot and root message handler
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("%s, I heard: %s", session.userData.name, session.message.text);
+    session.send("Say 'help' or something else...");
 });
 
-const server = restify.createServer();
-server.post('/api/messages', bot.connector('*').listen());
-server.listen(process.env.PORT, () => {
-    console.log(`${server.name} listening to ${server.url}`);
-});
-
-
-//=========================================================
-// Conversation Structure - Dialog Labels
-//=========================================================
-
-var DialogLabels = {
-    Hair: 'Hair',
-    Colour: 'Colour',
-    Spa: 'Spa',
-    Skin: 'Skin',
-    Tint: 'Tint'
-};
-
-
-
-
-//=========================================================
-// Conversation Structure - MAIN
-//=========================================================
-
-var bot = new builder.UniversalBot(connector, [
+// Add first run dialog
+bot.dialog('firstRun', [
     function (session) {
-        session.beginDialog('/banner');
-        session.beginDialog('/rootMenu');
+        // Update versio number and start Prompts
+        // - The version number needs to be updated first to prevent re-triggering 
+        //   the dialog. 
+        session.userData.version = 1.0; 
+        builder.Prompts.text(session, "Hello... What's your name?");
     },
     function (session, results) {
-        session.endConversation("Goodbye until next time...");
+        // We'll save the users name and send them an initial greeting. All 
+        // future messages from the user will be routed to the root dialog.
+        session.userData.name = results.response;
+        session.endDialog("Hi %s, say something to me and I'll echo it back.", session.userData.name); 
     }
-
-]);
-
-
-
-// Banner =================================================
-bot.dialog('/banner', [
-    function (session) {
-        // Send a greeting and show help.
-        var card = new builder.HeroCard(session)
-            .title("Hello welcome to Hairhaus - London Ontario")
-            .images([
-                 builder.CardImage.create(session, "http://hairhaus.ca/images/hair-haus-logo-white.jpg")
-            ]);
-        var msg = new builder.Message(session).attachments([card]);
-        session.send(msg);
+]).triggerAction({
+    onFindAction: function (context, callback) {
+        // Trigger dialog if the users version field is less than 1.0
+        // - When triggered we return a score of 1.1 to ensure the dialog is always triggered.
+        var ver = context.userData.version || 0;
+        var score = ver < 1.0 ? 1.1: 0.0;
+        callback(null, score);
+    },
+    onInterrupted: function (session, dialogId, dialogArgs, next) {
+        // Prevent dialog from being interrupted.
+        session.send("Sorry... We need some information from you first.");
     }
-]);
-
-
-
-//=========================================================
-// Conversation Actions
-//=========================================================
-
-bot.dialog('hair', require('./hair'))
-    .triggerAction({
-        matches: [/cut/i,/hair/i]
-    });
-bot.dialog('colour', require('./colour'))
-    .triggerAction({
-        matches: [/colour/i, /color/i]
-    });
-bot.dialog('spa', require('./spa'))
-    .triggerAction({
-        matches: [/spa/i]
-    });
-bot.dialog('skin', require('./skin'))
-    .triggerAction({
-        matches: [/skin/i]
-    });
-bot.dialog('tint', require('./tint'))
-    .triggerAction({
-        matches: [/tint/i]
-    });
-bot.dialog('support', require('./support'))
-    .triggerAction({
-        matches: [/help/i, /support/i, /problem/i]
-    });
-
-//=========================================================
-// Error logging
-//=========================================================
-
-bot.on('error', function (e) {
-    console.log('An error has ocurred: ', e);
 });
+
+// Add help dialog
+bot.dialog('help', function (session) {
+    session.send("I'm a simple echo bot.");
+}).triggerAction({ matches: /^help/i });
