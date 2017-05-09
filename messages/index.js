de1eb4ee51ff8acc4f2525760bdb97ed91075404
warren <warren.helland@hyparec.com>
2017-05-09 18:00:08 -0400

    updating/adding files

--- messages/index.js
+++ messages/index.js
@@ -1,48 +1,11 @@
 
-var builder = require("botbuilder");
-var botbuilder_azure = require("botbuilder-azure");
 const restify = require('restify');
-
-
-var useEmulator = (process.env.NODE_ENV == 'development');
-
-var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    stateEndpoint: process.env['BotStateEndpoint'],
-    openIdMetadata: process.env['BotOpenIdMetadata']
-});
-
+require('dotenv').config();
+const bot = require('./bot.js');
 
 const server = restify.createServer();
 server.post('/api/messages', bot.connector('*').listen());
 server.listen(process.env.PORT, () => {
     console.log(`${server.name} listening to ${server.url}`);
 });
-
-
-
-
-// Setup bot and root waterfall
-//var connector = new builder.ConsoleConnector().listen();
-var bot = new builder.UniversalBot(connector, [
-    function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                     " you've been programming for " + session.userData.coding + 
-                     " years and use " + session.userData.language + ".");
-    }
-]);
-
 
