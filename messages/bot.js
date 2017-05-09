aa9617f78fc6bc01c1e7d8deae5dd554a2b90459
warren <warren.helland@hyparec.com>
2017-05-09 18:17:40 -0400

    updating/adding files

--- messages/bot.js
+++ messages/bot.js
@@ -3,7 +3,12 @@
 //=========================================================
 
 'use strict';
+const builder = require('botbuilder');
 
+const connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 
 
 //=========================================================
