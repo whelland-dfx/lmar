be7a8a9dd460119f4a07fdf40809bd35e26b770e
warren <warren.helland@hyparec.com>
2017-05-09 19:10:53 -0400

    updating/adding files

--- messages/index.js
+++ messages/index.js
@@ -7,7 +7,14 @@ require('dotenv-extended').load();
 
 const server = restify.createServer();
 server.post('/api/messages', bot.connector('*').listen());
+
+/*
 server.listen(process.env.PORT, () => {
     console.log(`${server.name} listening to ${server.url}`);
 });
+*/
 
+
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
