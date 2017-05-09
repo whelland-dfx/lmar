cb5c367ee71df9826113767abac62497c19c0e32
warren <warren.helland@hyparec.com>
2017-05-09 19:37:46 -0400

    updating/adding files

--- messages/colour.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (session) {
-    session.error('Colour Dialog is not implemented and is currently set to throw Bot error handling');
-};
--- messages/tint.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (session) {
-    session.error('Tint Dialog is not implemented and is currently set to throw Bot error handling');
-};
