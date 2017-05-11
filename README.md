cc014a9034271dbfaca95ee3662894d2bb962119
warren <warren.helland@hyparec.com>
2017-05-10 23:25:30 -0400

    Changed stuffg

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# hairhaus
+# lmar
--- messages/index.js
+++ messages/index.js
@@ -1,98 +1,72 @@
-/*-----------------------------------------------------------------------------
-This demo shows how to create a progress dialog that will periodically notify
-users of the status of a long running task.
------------------------------------------------------------------------------*/
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
 
-var restify = require('restify');
 var builder = require('botbuilder');
+var restify = require('restify');
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
-  
-// Create chat connector for communicating with the Bot Framework Service
+
+// Create chat bot and listen to messages
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-
-// Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// Create your bot with a function to receive messages from the user.
+var DialogLabels = {
+    Hotels: 'Hotels',
+    Flights: 'Flights',
+    Support: 'Support'
+};
+
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-        // We can wrap a potentially long running operation with a progress dialog. This
-        // will periodically notify the user that the bot is still working on their 
-        // request. 
-        var options = {
-            initialText: 'Please wait... This may take a few moments.',
-            text: 'Please wait... This is taking a little longer then expected.',
-            speak: '<speak>Please wait.<break time="2s"/></speak>'
-        };
-        progress(session, options, function (callback) {
-            // Make our async call here. If the call completes quickly then no progress
-            // message will be sent.
-            setTimeout(function () {
-                callback('You said: ' + session.message.text);
-            }, 20000);
-        });
+        // prompt for search option
+        builder.Prompts.choice(
+            session,
+            'Are you looking for a flight or a hotel?',
+            [DialogLabels.Flights, DialogLabels.Hotels],
+            {
+                maxRetries: 3,
+                retryPrompt: 'Not a valid option'
+            });
     },
-    function (session, results) {
-        session.send(results.response);
+    function (session, result) {
+        if (!result.response) {
+            // exhausted attemps and no selection, start over
+            session.send('Ooops! Too many attemps :( But don\'t worry, I\'m handling that exception and you can try again!');
+            return session.endDialog();
+        }
+
+        // on error, start over
+        session.on('error', function (err) {
+            session.send('Failed with message: %s', err.message);
+            session.endDialog();
+        });
+
+        // continue on proper dialog
+        var selection = result.response.entity;
+        switch (selection) {
+            case DialogLabels.Flights:
+                return session.beginDialog('flights');
+            case DialogLabels.Hotels:
+                return session.beginDialog('hotels');
+        }
     }
 ]);
 
-/**
- * Wrapper function to simplify calling our progress dialog.
- * @param {*} session Current session object.  
- * @param {*} options Options to control messages sent by the progress dialog. 
- *      Values:
- *          * text:         (Required) progress text to send.
- *          * speak:        (Optional) ssml to send with each progress message.
- *          * delay:        (Optional) delay (in ms) before progress is sent.
- *          * initialText:  (Optional) initial progress text.
- *          * initialSpeak: (Optional) initial progress ssml.
- *          * initialDelay: (Optional) delay before initial progress is sent.
- * @param {*} asyncFn Async function to call. Will be passed a callback with a 
- *      signature of (response: any) => void.  
- */
-function progress(session, options, asyncFn) {
-    session.beginDialog('progressDialog', {
-        asyncFn: asyncFn,
-        options: options
-    })
-}
+bot.dialog('flights', require('./flights'));
+bot.dialog('hotels', require('./hotels'));
+bot.dialog('support', require('./support'))
+    .triggerAction({
+        matches: [/help/i, /support/i, /problem/i]
+    });
 
-bot.dialog('progressDialog', function (session, args) {
-    var asyncFn = args.asyncFn;
-    var options = args.options;
-
-    var count = 0;
-    function sendProgress() {
-        if (count++ > 0) {
-            session.say(options.text, options.speak, { inputHint: builder.InputHint.ignoringInput });
-        } else {
-            var text = options.initialText || options.text;
-            var speak = options.initialSpeak || options.speak;
-            session.say(text, speak, { inputHint: builder.InputHint.ignoringInput });
-        }
-        hTimer = setTimeout(sendProgress, options.delay || 9000);
-    }
-
-    // Start progress timer
-    var hTimer = setTimeout(sendProgress, options.initialDelay || 1000);
-
-    // Call async function
-    try {
-        asyncFn(function (response) {
-            // Stop timer and return response
-            clearTimeout(hTimer);
-            session.endDialogWithResult({ response: response });
-        });
-    } catch (err) {
-        session.error(err);
-    }
+// log any bot errors into the console
+bot.on('error', function (e) {
+    console.log('And error ocurred', e);
 });
